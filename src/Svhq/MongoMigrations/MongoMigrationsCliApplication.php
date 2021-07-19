6589a543ccfd17439eaac1361c9632fc80506a03
Anders Krarup <andkrup@gmail.com>
2021-07-19 09:53:57 +0200

    the other key

--- src/Svhq/MongoMigrations/MongoMigrationsCliApplication.php
+++ src/Svhq/MongoMigrations/MongoMigrationsCliApplication.php
@@ -72,7 +72,7 @@ namespace Svhq\MongoMigrations {
                     self::$logCollection = trim($migrationOverrides->entries);
                 }
             }
-            DependencyLoader::loadFromConfig(Config::instance(self::$overrideKey), 'dependencies');
+            DependencyLoader::loadFromConfig(Config::instance($instance->key()), 'dependencies');
 
             if(!is_writable(self::$migrationDir) || !is_dir(self::$migrationDir)){
                 $path = self::$migrationDir;
